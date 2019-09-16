c3aa64ce7686acb3d954259edd9d05acb81c3912
Alexander <alexander.de.haan@student.nhlstenden.com>
2019-09-16 11:42:24 +0200

    V0.1.6 New xaml and classes

---
+++ Memory Project/Memory Project/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Project
+{
+    class Board
+    {
+        private int height;
+        private int width;
+
+        public Board(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+    }
+}
