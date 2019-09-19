50f8a0f5b6deecedd83fba893de2e5756d1c6582
Alexander <alexander.de.haan@student.nhlstenden.com>
2019-09-19 19:55:36 +0200

    The Overhaul has begun, need player class integration into gamecontroller. need name input. need turn handling.

---
+++ Memory Project/Memory Project/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Project
+{
+    class Player
+    {
+        string name;
+        int score;
+        List<Card> collectedCards = new List<Card>();
+
+        public Player(string name)
+        {
+            this.name = name;
+            score = 0;
+            collectedCards.Clear();
+        }
+    }
+}
