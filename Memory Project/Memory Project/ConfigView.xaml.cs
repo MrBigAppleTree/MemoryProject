14f82b5e0a65bdbb9e920863e6a77d492f9d91ee
Thomas Coers <thomas.coers@student.nhlstenden.com>
2019-10-28 13:31:07 +0100

    ConfigView - added fullscreen, removed resolution option, fixed checkbox bug

--- Memory Project/Memory Project/ConfigView.xaml.cs
+++ Memory Project/Memory Project/ConfigView.xaml.cs
@@ -22,12 +22,13 @@ namespace Memory_Project
     public partial class ConfigView : Page
     {
         public string currentTheme;
-        public bool checkBoxState { get; set; }
         public ConfigView()
         {
+
             InitializeComponent();
+            FullscreenToggle.IsChecked = (bool)Application.Current.Resources["FullscreenToggle"];
+            SoundToggle.IsChecked = (bool)Application.Current.Resources["MusicToggle"];
             currentTheme = (string)Application.Current.Resources["Theme"];
-            checkBoxState = (bool)Application.Current.Resources["MusicToggle"]; //page change doesn't keep visual checked value
             BackgroundImg.Source = new BitmapImage(new Uri(@"../../images/" + currentTheme + "/MenuBackground.png", UriKind.Relative));
         }
         private void back_click(object sender, RoutedEventArgs e)
@@ -36,16 +37,6 @@ namespace Memory_Project
             this.NavigationService.Navigate(new Uri("MainNav.xaml", UriKind.Relative));
 
         }
-        private void theme_click(object sender, RoutedEventArgs e)
-        {
-            this.NavigationService.Navigate(new Uri("ThemeNav.xaml", UriKind.Relative));
-        }
-
-        private void close_click(object sender, RoutedEventArgs e)
-        {
-            System.Environment.Exit(1);
-        }
-
         private void SoundToggle_Checked(object sender, RoutedEventArgs e)
         {
             Application.Current.Resources["MusicToggle"] = true;
@@ -57,32 +48,46 @@ namespace Memory_Project
             Application.Current.Resources["MusicToggle"] = false;
             MusicToggle(currentTheme);
         }
-
-
-        //Changed to always fullscreen
-
-        private void Resolution_DropDownClosed(object sender, EventArgs e)
+        private void FullscreenToggle_Checked(object sender, RoutedEventArgs e)
         {
-            switch (Resolution.SelectedIndex)
-            {
-                case 0:
-                    Application.Current.Resources["Width"] = 1280;
-                    Application.Current.Resources["Height"] = 720;
-                    break;
-                case 1:
-                    Application.Current.Resources["Width"] = 1600;
-                    Application.Current.Resources["Height"] = 900;
-                    break;
-                case 2:
-                    Application.Current.Resources["Width"] = 1920;
-                    Application.Current.Resources["Height"] = 1080;
-                    break;
-
-            }
-            Application.Current.MainWindow.Width = 1600;
-            Application.Current.MainWindow.Height = 900;
-            //this.NavigationService.Refresh();
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            Application.Current.MainWindow.WindowStyle = WindowStyle.None;
+            Application.Current.MainWindow.ResizeMode = ResizeMode.NoResize;
+            Application.Current.Resources["FullscreenToggle"] = true;
         }
+        private void FullscreenToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+            Application.Current.MainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+            Application.Current.MainWindow.ResizeMode = ResizeMode.CanResize;
+            Application.Current.Resources["FullscreenToggle"] = false ;
+        }
+
+        //Changed to always fullscreen/same Resolution
+
+        //private void Resolution_DropDownClosed(object sender, EventArgs e)
+        //{
+        //    switch (Resolution.SelectedIndex)
+        //    {
+        //        case 0:
+        //            Application.Current.Resources["Width"] = 1280;
+        //            Application.Current.Resources["Height"] = 720;
+        //            break;
+        //        case 1:
+        //            Application.Current.Resources["Width"] = 1600;
+        //            Application.Current.Resources["Height"] = 900;
+        //            WindowHeight = (double)Application.Current.Resources["Height"];
+        //            WindowWidth = (double)Application.Current.Resources["Width"];
+        //            MessageBox.Show(Application.Current.Resources["Width"].ToString());
+        //            break;
+        //        case 2:
+        //            Application.Current.Resources["Width"] = 1920;
+        //            Application.Current.Resources["Height"] = 1080;
+        //            break;
+
+        //    }
+
+        //}
         public void MusicToggle(string theme)
         {
             bool musicToggle = (bool)Application.Current.Resources["MusicToggle"];
