d6f29d9ee9d158e45cbd12f9a0852a2c64c44216
ErikPapa <Esander970@gmail.com>
2019-11-01 10:35:43 +0100

    Added comments.

--- Memory Project/Memory Project/MainNav.xaml.cs
+++ Memory Project/Memory Project/MainNav.xaml.cs
@@ -48,26 +48,43 @@ namespace Memory_Project
             System.Environment.Exit(1);
         }
 
+        //Wordt uit gevoerd zodra een van de dropdowns van de highscore pagina gesloten wordt.
         private void Mode_Change(object sender, EventArgs e)
         {
+            //Hier wordt gekeken of er al highscores op de pagina staan, als dit het geval is dan wordt deze uigevoerd.
             if (HighGrid.Children.Count > 0)
             {
+                //Hier haalt hij de highscore pagina leeg zodat er weer een nieuwe op kan staan, anders heb je namelijk meerdere highscores door elkaar staan.
                 HighGrid.Children.Clear();
             }
             try
             {
+                //playercount wordt gehaald uit de dropdown van het aantal spelers dat er mee speelde aan het spel, hier wordt +1 gedaan omdat hij bij 0 begint met de index.
                 int playercount = hiscr.SelectedIndex + 1;
+                //height wordt weg gehaald uit de eerste van de twee cijfers op de highscores pagina, hier wordt +2 gedaan omdat de index bij 0 begint en de eerste van de lijst is 2.
                 int height = Height.SelectedIndex + 2;
+                //width wordt weg gehaald uit de tweede van de twee cijfers op de highscores pagina, hier wordt +2 gedaan omdat de index bij 0 begint en de eerste van de lijst is 2.
                 int width = Width.SelectedIndex + 2;
+                //Het maakt niet uit voor de highscore page of je 3x2 of 2x3 doet want hij kijkt naar het aantal kaarten en dat is voor zowel 3x2 als 2x3 6, het spel zelf is dan ook het zelfde dus het maakt niet echt uit.
+
+
+                //size is hieght*width, zo weet je dus hoeveel kaarten er op het speelveld staat
                 int size = height * width;
+
+                //loadmode is het zelfde als "mode" van highscores.cs, hier wordt het aantal spelers voor "player_" gezet met daar achter het aantal kaarten op het speelveld.
                 string loadmode = playercount + "player_" + size;
                 HighScores h = new HighScores();
+                //Hier zet hij de highscores van Highscores in een list.
                 List<KeyValuePair<string, int>> HighScreen = h.MainDic[loadmode].ToList();
+                //Hier kijkt hij naar het aantal highscores in highscreen.
                 for (int i = 0; i < HighScreen.Count; i++)
                 {
                     try
                     {
+                        //Hier maakt hij een nieuwe row aan.
                         HighGrid.RowDefinitions.Add(new RowDefinition());
+
+                        //Hier wordt de positie van de score aangegeven met een int, de eerste is 1 en gaat met 1 omhoog voor elke speler met een score.
                         TextBlock Position = new TextBlock();
                         Position.TextAlignment = TextAlignment.Left;
                         Position.FontSize = 25;
@@ -77,6 +94,7 @@ namespace Memory_Project
                         Position.Text = i + 1 + ".";
                         HighGrid.Children.Add(Position);
 
+                        //Hier zet hij de naam van de speler neer naast de positie.
                         TextBlock PlayerName = new TextBlock();
                         PlayerName.TextAlignment = TextAlignment.Left;
                         PlayerName.FontSize = 25;
@@ -86,6 +104,7 @@ namespace Memory_Project
                         PlayerName.Text = HighScreen[i].Key;
                         HighGrid.Children.Add(PlayerName);
 
+                        //Hier zet hij de score van de speler neer van de speler, dit zet hij naast de speler neer.
                         TextBlock Scores = new TextBlock();
                         Scores.TextAlignment = TextAlignment.Left;
                         Scores.FontSize = 25;
@@ -95,26 +114,16 @@ namespace Memory_Project
                         Scores.Text = Convert.ToString(HighScreen[i].Value);
                         HighGrid.Children.Add(Scores);
                     }
+                    //Als er geen highscores zijn voor een gamemode dan staat er ook niks op de highscore pagina.
                     catch
                     {
                     }
                 }
             }
+            //Als er geen highscores zijn voor een gamemode dan staat er ook niks op de highscore pagina.
             catch
             {
             }
-        }
-
-        private void startMusic(string theme)
-        {
-            try
-            {
-                SoundPlayer player = new SoundPlayer();
-                player.SoundLocation = "music/" + theme + "/BackgroundMusic.wav";
-                player.PlayLooping();
-            }
-            catch (Exception e) { }
-            
         }
     }
 }
